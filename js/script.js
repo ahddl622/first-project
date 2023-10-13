5b96db9d0be4e9a628014a4fd9b3394c36f165e8
[Github <[Github>
2023-10-14 03:14:25 +0900

    Fix toggle bug

--- js/script.js
+++ js/script.js
@@ -1,51 +1,40 @@
-  const els = document.getElementsByClassName("el")
+const els = document.getElementsByClassName("el");
 
+window.addEventListener("scroll", function () {
+  const vh = window.innerHeight;
 
-  window.addEventListener("scroll", function() {
+  for (let i = 0; i < els.length; i++) {
+    const y = els[i].getBoundingClientRect().y;
 
-  const vh = window.innerHeight
-
-    for (let i=0; i<els.length; i++) {
-
-      const y = els[i].getBoundingClientRect().y
-
-      if (y < vh * 0.9) {
-        els[i].classList.remove("opacity-0")
-      }
+    if (y < vh * 0.9) {
+      els[i].classList.remove("opacity-0");
     }
-  })
-
-  document.addEventListener("DOMContentLoaded", function() {
-    const text = document.querySelector('.backimage');
-    
-    setTimeout(() => {
-      text.style.opacity = 1;
-    }, 1000);
-  });
-
-
-  
-
-document.addEventListener("DOMContentLoaded", function() {
-  const textElements = document.querySelectorAll('.hidden-text');
-  
-  setTimeout(() => {
-    textElements.forEach(text => {
-      text.style.opacity = 1;
-    });
-  }, 100); 
+  }
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const text = document.querySelector(".backimage");
 
+  setTimeout(() => {
+    text.style.opacity = 1;
+  }, 1000);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const textElements = document.querySelectorAll(".hidden-text");
+
+  setTimeout(() => {
+    textElements.forEach((text) => {
+      text.style.opacity = 1;
+    });
+  }, 100);
+});
 
 function toggleSubcategory(subcatId) {
-  const subcategoryList = document.getElementById(subcatId);
-  if (subcategoryList.style.display === 'none') {
-    subcategoryList.style.display = 'block';
+  const subcategory = document.getElementById(subcatId);
+  if (subcategory.style.display === "none" || !subcategory.style.display) {
+    subcategory.style.display = "block";
   } else {
-    subcategoryList.style.display = 'none';
+    subcategory.style.display = "none";
   }
 }
-
-
-
